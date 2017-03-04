b7ad2b29a310c1278b61f007e5f8876682cc5005
Benjamin Zagorski <benj@trustsherpa.com>
2017-03-03 20:12:50 -0800

    Tests moved to test.js

--- script.js
+++ script.js
@@ -17,74 +17,6 @@ function makeMove(column) {
   switchTurn();
 }
 
-function horVicTest() {
-  makeMove(1);
-  makeMove(1);
-  makeMove(2);
-  makeMove(2);
-  makeMove(3);
-  makeMove(3);
-  makeMove(4);
-}
-
-function diagVicTest1() {
-  makeMove(0); // blue
-  makeMove(1); // red
-  makeMove(1); // blue
-  makeMove(2); // red
-  makeMove(2); // blue
-  makeMove(3); // red
-  makeMove(2); // blue
-  makeMove(3); // red
-  makeMove(3); // blue
-  makeMove(4); // red
-  makeMove(3); // blue
-}
-
-function diagVicTest2() {
-  makeMove(4); // blue
-  makeMove(2); // red
-  makeMove(3); // blue
-  makeMove(2); // red
-  makeMove(3); // blue
-  makeMove(1); // red
-  makeMove(2); // blue
-  makeMove(1); // red
-  makeMove(1); // blue
-  makeMove(5); // red
-  makeMove(1); // blue
-}
-
-function diagVicTest3() {
-  makeMove(6); // blue
-  makeMove(6); // red
-  makeMove(6); // blue
-  makeMove(6); // red
-  makeMove(5); // blue
-  makeMove(5); // red
-  makeMove(4); // blue
-  makeMove(5); // red
-  makeMove(1); // blue
-  makeMove(4); // red
-  makeMove(1); // blue
-  makeMove(3); // red
-}
-function diagVicTest4() {
-  makeMove(6); // blue
-  makeMove(6); // red
-  makeMove(6); // blue
-  makeMove(6); // red
-  makeMove(5); // blue
-  makeMove(5); // red
-  makeMove(4); // blue
-  makeMove(5); // red
-  makeMove(1); // blue
-  makeMove(3); // red
-  makeMove(1); // blue
-  makeMove(4); // red
-}
-
-
 function checkForVictory(column) {
   var row = document.columns[column].length - 1;
 
@@ -222,9 +154,3 @@ function switchTurn() {
     console.log("TURN ERROR");
   }
 }
-
-// function recallibratePage() {
-//   document.columns.forEach(function(column, ind, arr) {
-//
-//   })
-// }
---
+++ test.js
@@ -0,0 +1,67 @@
+
+function horVicTest() {
+  makeMove(1);
+  makeMove(1);
+  makeMove(2);
+  makeMove(2);
+  makeMove(3);
+  makeMove(3);
+  makeMove(4);
+}
+
+function diagVicTest1() {
+  makeMove(0); // blue
+  makeMove(1); // red
+  makeMove(1); // blue
+  makeMove(2); // red
+  makeMove(2); // blue
+  makeMove(3); // red
+  makeMove(2); // blue
+  makeMove(3); // red
+  makeMove(3); // blue
+  makeMove(4); // red
+  makeMove(3); // blue
+}
+
+function diagVicTest2() {
+  makeMove(4); // blue
+  makeMove(2); // red
+  makeMove(3); // blue
+  makeMove(2); // red
+  makeMove(3); // blue
+  makeMove(1); // red
+  makeMove(2); // blue
+  makeMove(1); // red
+  makeMove(1); // blue
+  makeMove(5); // red
+  makeMove(1); // blue
+}
+
+function diagVicTest3() {
+  makeMove(6); // blue
+  makeMove(6); // red
+  makeMove(6); // blue
+  makeMove(6); // red
+  makeMove(5); // blue
+  makeMove(5); // red
+  makeMove(4); // blue
+  makeMove(5); // red
+  makeMove(1); // blue
+  makeMove(4); // red
+  makeMove(1); // blue
+  makeMove(3); // red
+}
+function diagVicTest4() {
+  makeMove(6); // blue
+  makeMove(6); // red
+  makeMove(6); // blue
+  makeMove(6); // red
+  makeMove(5); // blue
+  makeMove(5); // red
+  makeMove(4); // blue
+  makeMove(5); // red
+  makeMove(1); // blue
+  makeMove(3); // red
+  makeMove(1); // blue
+  makeMove(4); // red
+}
